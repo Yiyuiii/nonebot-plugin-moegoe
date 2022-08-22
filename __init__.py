c746f2623522a53fcc773ea65dc82ac60c7409c2
yiyuiii <yiyuiii@foxmail.com>
2022-08-22 17:13:26 +0800

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -11,12 +11,12 @@ __plugin_meta__ = PluginMetadata(
     usage="moegoe\n" +
           "- 让[宁宁|爱瑠|芳乃|茉子|丛雨|小春|七海]说日语：(日语)\n" +
           "- 让[Sua|Mimiru|Arin|Yeonhwa|Yuhwa|Seonbae]说韩语：(韩语)\n" +
-          "- 让[派蒙|凯亚|安柏|丽莎|琴|香菱|枫原万叶|迪卢克|温迪|可莉|早柚|托马|芭芭拉|优菈|云堇|钟离|魈|凝光|雷电将军|北斗|甘雨|七七|刻晴|神里绫华|雷泽|神里绫人|罗莎莉亚|阿贝多|八重神子|宵宫|荒泷一斗|九条裟罗|夜兰|珊瑚宫心海|五郎|达达利亚|莫娜|班尼特|申鹤|行秋|烟绯|久岐忍|辛焱|砂糖|胡桃|重云|菲谢尔|诺艾尔|迪奥娜|鹿野院平藏]说中文：(中文)",
+          "- 让[派蒙|凯亚|安柏|丽莎|琴|香菱|枫原万叶|迪卢克|温迪|可莉|早柚|托马|芭芭拉|优菈|云堇|钟离|魈|凝光|雷电将军|北斗|甘雨|七七|刻晴|神里绫华|雷泽|神里绫人|罗莎莉亚|阿贝多|八重神子|宵宫|荒泷一斗|九条裟罗|夜兰|珊瑚宫心海|五郎|达达利亚|莫娜|班尼特|申鹤|行秋|烟绯|久岐忍|辛焱|砂糖|胡桃|重云|菲谢尔|诺艾尔|迪奥娜|鹿野院平藏]说(中文)",
     extra={
         "unique_name": "moegoe",
-        "example": "让派蒙说中文：你好",
+        "example": "让派蒙说你好！旅行者。",
         "author": "yiyuiii <yiyuiii@foxmail.com>",
-        "version": "0.3.0",
+        "version": "0.4.0",
     },
 )
 
@@ -41,13 +41,14 @@ def cn_func(msg, name='派蒙'):
     return MessageSegment.record(cnapi.substitute(text=msg, id=name))
 
 
-jp_regex = "^让(宁宁|爱瑠|芳乃|茉子|丛雨|小春|七海)说日语：(.+)$"
-kr_regex = "^让(Sua|Mimiru|Arin|Yeonhwa|Yuhwa|Seonbae)说韩语：(.+)$"
-cn_regex = "^让(派蒙|凯亚|安柏|丽莎|琴|香菱|枫原万叶|迪卢克|温迪|可莉|早柚|托马|芭芭拉|优菈|云堇|钟离|魈|凝光|雷电将军|北斗|甘雨|七七|刻晴|神里绫华|雷泽|神里绫人|罗莎莉亚|阿贝多|八重神子|宵宫|荒泷一斗|九条裟罗|夜兰|珊瑚宫心海|五郎|达达利亚|莫娜|班尼特|申鹤|行秋|烟绯|久岐忍|辛焱|砂糖|胡桃|重云|菲谢尔|诺艾尔|迪奥娜|鹿野院平藏)说中文：(.+)$"
+jp_regex = "^让(宁宁|爱瑠|芳乃|茉子|丛雨|小春|七海)说(?:日语|日文|日本语)：(.+)$"
+kr_regex = "^让(Sua|Mimiru|Arin|Yeonhwa|Yuhwa|Seonbae)说(?:韩语|韩文|韩国语)：(.+)$"
+cn_regex = "^让(派蒙|凯亚|安柏|丽莎|琴|香菱|枫原万叶|迪卢克|温迪|可莉|早柚|托马|芭芭拉|优菈|云堇|钟离|魈|凝光|雷电将军|北斗|甘雨|七七|刻晴|神里绫华|雷泽|神里绫人|罗莎莉亚|阿贝多|八重神子|宵宫|荒泷一斗|九条裟罗|夜兰|珊瑚宫心海|五郎|达达利亚|莫娜|班尼特|申鹤|行秋|烟绯|久岐忍|辛焱|砂糖|胡桃|重云|菲谢尔|诺艾尔|迪奥娜|鹿野院平藏)说(.+)$"
 
-jp_cmd = on_regex(jp_regex, block=True, priority=5)
-kr_cmd = on_regex(kr_regex, block=True, priority=5)
-cn_cmd = on_regex(cn_regex, block=True, priority=5)
+Priority = 5
+jp_cmd = on_regex(jp_regex, block=True, priority=Priority)
+kr_cmd = on_regex(kr_regex, block=True, priority=Priority)
+cn_cmd = on_regex(cn_regex, block=True, priority=Priority)
 
 
 @jp_cmd.handle()
