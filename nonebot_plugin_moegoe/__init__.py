5be0e5edfdefbc1c975e6076024fb1cc8cbada0f
yiyuiii <yiyuiii@foxmail.com>
2023-11-08 02:09:12 +0800

    0.9.0

--- nonebot_plugin_moegoe/__init__.py
+++ nonebot_plugin_moegoe/__init__.py
@@ -214,18 +214,10 @@ async def cn_func(
     return await get_MessageSegment(url, name, msg, output_format)
 
 
-jp_cmd = on_regex(
-    profileDict["jpapi"]["regex"], block=True, priority=profileDict["priority"]
-)
-jp2_cmd = on_regex(
-    profileDict["jp2api"]["regex"], block=True, priority=profileDict["priority"]
-)
-kr_cmd = on_regex(
-    profileDict["krapi"]["regex"], block=True, priority=profileDict["priority"]
-)
-cn_cmd = on_regex(
-    profileDict["cnapi"]["regex"], block=True, priority=profileDict["priority"]
-)
+jp_cmd = on_regex(profileDict["jpapi"]["regex"], block=True, priority=profileDict["priority"])
+jp2_cmd = on_regex(profileDict["jp2api"]["regex"], block=True, priority=profileDict["priority"])
+kr_cmd = on_regex(profileDict["krapi"]["regex"], block=True, priority=profileDict["priority"])
+cn_cmd = on_regex(profileDict["cnapi"]["regex"], block=True, priority=profileDict["priority"])
 
 
 async def msg_process(matcher: Matcher, matched: Tuple[Any, ...], api_func):
